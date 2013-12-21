cd62d8ab5efc797e2f9572989f246afb0d3c1b2c
molin0827 <kgd8023yxq@gmail.com>
2013-12-21 10:03:59 +0800

    第一次提交

---
+++ PromptSound/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace PromptSound
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(36, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "PromptSound";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(67, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "试听";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 77);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "PromptSound";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PromptSound/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Media;
+using System.Windows.Forms;
+using System.IO;
+using System.Web;
+
+namespace PromptSound
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://5.emopshop.sinaapp.com/SyncData/get_verify_count";
+            Encoding code = Encoding.GetEncoding("utf-8");
+            WebRequest wr = WebRequest.Create(url);
+            wr.Credentials = CredentialCache.DefaultCredentials;
+            wr.Timeout = 20000;
+            WebResponse response;
+            response = wr.GetResponse();
+            string html;
+            html = new StreamReader(response.GetResponseStream(), code).ReadToEnd();
+            //html = "0";
+            //label1.Text = html;
+            
+            int a = int.Parse(html);
+            if (a > 0)
+            {
+                SoundPlayer sp = new SoundPlayer();
+                sp.SoundLocation = "msg.wav";
+                sp.Load();
+                sp.Play();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                //Console.Out.WriteLine("this my write");
+                string url = "http://5.emopshop.sinaapp.com/SyncData/get_verify_count";
+                Encoding code = Encoding.GetEncoding("utf-8");
+                WebRequest wr = WebRequest.Create(url);
+                wr.Credentials = CredentialCache.DefaultCredentials;
+                wr.Timeout = 20000;
+                WebResponse response;
+                response = wr.GetResponse();
+                string html;
+                html = new StreamReader(response.GetResponseStream(), code).ReadToEnd();
+                //html = "0";
+                //label1.Text = html;
+
+                int a = int.Parse(html);
+                if (a > 0)
+                {
+                    SoundPlayer sp = new SoundPlayer();
+                    sp.SoundLocation = "msg.wav";
+                    sp.Load();
+                    sp.Play();
+                }
+            }
+            catch( Exception ex)
+            {
+                Console.Out.WriteLine(ex);
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //注意判断关闭事件Reason来源于窗体按钮，否则用菜单退出时无法退出!
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;    //取消"关闭窗口"事件
+                this.WindowState = FormWindowState.Minimized;    //使关闭时窗口向右下角缩小的效果
+                notifyIcon1.Visible = true;
+                this.Hide();
+                return;
+            }
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
